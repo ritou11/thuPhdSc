99c2daa7fb0e05ccc588e12595e0cad1479849e6
haotian <ritou11@gmail.com>
2018-09-19 17:35:35 +0800

    select course

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const cheerio = require('cheerio');
 
 const { username, password } = require('./secret.json');
 
+const SEMESTER = '2018-2019-1';
+const TARGET_KCH = '60240103';
+const TARGET_KXH = '0';
+
 const info = (i) => console.info(i);
 
 const jar = new CookieJar();
@@ -30,6 +34,7 @@ const parseXwk = (xwkData) => {
   const xwkTotal = $('.trr2').length;
   const dict = ['课程号', '课序号', '课程名', '课余量', '上课时间', '课容量', '学分', '任课教师', '说明'];
   const res = [];
+
   for (let i = 1; i <= xwkTotal; i += 1) {
     const trRes = {};
     $(`#tr_${i}`).children().each((j, el) => {
@@ -38,6 +43,13 @@ const parseXwk = (xwkData) => {
     res.push(trRes);
   }
   return res;
+};
+
+const parseXkMsg = (resData) => {
+  const reg = /showMsg\("(.*?)"\)/gi;
+  const resM = reg.exec(resData);
+  if (resM.length >= 2) return resM[1];
+  return '';
 };
 
 const main = async () => {
@@ -71,7 +83,7 @@ const main = async () => {
     (await ax.get('xkYjs.vxkYjsXkbBs.do', {
       params: {
         m: 'xwkSearch',
-        p_xnxq: '2018-2019-1',
+        p_xnxq: SEMESTER,
         tokenPriFlag: 'xwk',
       },
       headers: {
@@ -81,9 +93,31 @@ const main = async () => {
     })).data, 'gb2312',
   );
   fs.writeFileSync('xwk.html', xwkData);
-  parseXwk(xwkData);
+
+  const $ = cheerio.load(xwkData);
+  const token = $('input[name="token"]').val();
+  const resData = iconv.decode((await ax.post('xkYjs.vxkYjsXkbBs.do',
+    querystring.stringify({
+      m: 'saveXwKc',
+      token,
+      p_xnxq: SEMESTER,
+      tokenPriFlag: 'xwk',
+      p_xwk_id: `${SEMESTER};${TARGET_KCH};${TARGET_KXH}`,
+    }), {
+      responseType: 'arraybuffer',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Referer: 'http://zhjwxk.cic.tsinghua.edu.cn/xklogin.do',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+      },
+    })).data, 'gb2312');
+  fs.writeFileSync('res.html', resData);
+
+  const resMsg = parseXkMsg(resData);
+  info(resMsg);
 };
 
-// main();
-const xwkData = fs.readFileSync('xwk.html');
-info(parseXwk(xwkData));
+main();
+
+// const s = fs.readFileSync('xwk.html');
+// parseXwk(s);
